a12b5ad13d28c745e995bb88d4e7d1c049111926
DK\mdo <mdo@sitecore.net>
2017-06-13 11:38:03 +0300

    Commit the patch

---
+++ src/Sitecore.Support.132442/Analytics/Pipelines/CreateVisits/XForwardedFor.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Analytics.Pipelines.CreateVisits;
+using Sitecore.Diagnostics;
+using System;
+using System.Linq;
+
+namespace Sitecore.Support.Analytics.Pipelines.CreateVisits
+{
+    public class XForwardedFor : Sitecore.Analytics.Pipelines.CreateVisits.XForwardedFor
+    {
+        protected override string GetIpFromHeader(string header)
+        {
+            Assert.ArgumentNotNull(header, "header");
+            string[] array = header.Split(new char[]
+            {
+                ','
+            });
+            int headerIpIndex = base.HeaderIpIndex;
+            string text = (headerIpIndex < array.Length) ? array[headerIpIndex] : array.LastOrDefault<string>();
+            if (string.IsNullOrEmpty(text))
+            {
+                return null;
+            }
+            string[] array2 = text.Split(new char[]
+            {
+                ':'
+            }, StringSplitOptions.RemoveEmptyEntries);
+            if (array2.Length > 1)
+            {
+                text = array2[0];
+            }
+            return text.Trim();
+        }
+    }
+}
---
+++ src/Sitecore.Support.132442/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.132442")]
+[assembly: AssemblyProduct("Sitecore.Support.132442")]
+[assembly: ComVisible(false)]
